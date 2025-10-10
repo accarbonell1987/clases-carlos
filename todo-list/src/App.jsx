0120492abe6bd8e3d1d62f5e91f0fd450d1aafc9
Alberto Carlos <albertocarlos.carbonell@weecover.com>
2025-10-09 22:13:29 -0300

    chore: base of project with structure and TODOS

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ItemList from './components/ItemList'
+import './styles/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Learn JavaScript', completed: false },
+    { id: 3, text: 'Learn HTML', completed: false },
+    { id: 4, text: 'Learn CSS', completed: false },
+  ])
+
+  const handleDelete = (id) => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
+  //TODO: Agregar funcionalidad de agregar tareas
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Todo List</h1>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        {tasks.map(task => (
+          <ItemList key={task.id} text={task.text} completed={task.completed} onDelete={() => handleDelete(task.id)} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ todo-list/src/components/ItemList.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const stlye = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+  padding: '10px',
+}
+
+//TODO: Diferenciar entre tareas completadas y no completadas
+// 1. Completadas con color de background gris
+// 2. No completadas con color de background orange
+
+//TODO: Adicionar funcion para modificar el completed
+
+const ItemList = ({ completed = false, text = '', onDelete = () => { } }) => {
+  return (
+    <div style={stlye}>
+      <input type="radio" checked={completed} />
+      <label>{text}</label>
+      <button onClick={onDelete}>Delete</button>
+    </div>
+  )
+}
+
+export default ItemList
