277d9d909131d76d2286695b529ce2d4bad747c2
Carlos Bens <carlosdbens@gmail.com>
2025-10-11 19:22:16 -0400

    add new project

---
+++ Task-List/src/App.jsx
@@ -0,0 +1,59 @@
+import { use, useEffect, useState } from "react";
+import "./App.css";
+import ToDOList from "./componentes/toDoList";
+
+function App() {
+  const [ischeck, setIsCheck] = useState(false);
+  const [count, setCount] = useState(0);
+  const [additem, setAdditem] = useState({ name: "" });
+  const [itemList, setItemList] = useState([]);
+  const remove = (id) => {
+    setItemList(itemList.filter((item) => item.id != id));
+  };
+
+  const handleClick = () => {
+    if (!additem.name || additem.name.trim() === "") return;
+    const newId = { id: count };
+    setItemList([...itemList, { ...additem, ...newId, completed: false }]);
+    setCount(count + 1);
+    setAdditem({ name: "" });
+  };
+  const handleChange = (e) => {
+    const newItem = e.target.value;
+    setAdditem({ name: newItem });
+  };
+  const handleCheck = (id) => {
+    const updateList = itemList.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+    setItemList(updateList);
+    setIsCheck(true);
+  };
+
+  return (
+    <div>
+      <h1>Item List</h1>
+      <input
+        type="text"
+        onChange={(e) => handleChange(e)}
+        value={additem.name}
+      />
+      <button onClick={handleClick} className="btn">
+        AddItem
+      </button>
+      <div>
+        {itemList.map((task) => (
+          <ToDOList
+            name={task.name}
+            remove={() => remove(task.id)}
+            key={task.id}
+            completed={task.completed}
+            handleCheck={() => handleCheck(task.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
